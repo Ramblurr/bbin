3ef56b294b9d9c67f83fcd0b414fed9ba57809bb
Radford Smith <11401+rads@users.noreply.github.com>
2025-11-20 20:17:40 -0800

    get non-dir tests passing

--- src/babashka/bbin/scripts/common.clj
+++ src/babashka/bbin/scripts/common.clj
@@ -334,7 +334,7 @@
 (defn add-deps [{:keys [script-deps] :as params}]
   (let [lib (key (first script-deps))]
     (when-not (#{::no-lib} lib)
-      (bbin-deps/add-deps {:deps script-deps}))
+      (bbin-deps/add-libs script-deps))
     params))
 
 (defn calc-script-root [{:keys [header script-deps] :as params}]
